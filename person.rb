2df8fdc13699e5902e560f4ebf6579948e1e1101
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-06-13 12:32:04 +0300

    Update included:   - Private method is_of_age?   - Public method can_use_services?

--- person.rb
+++ person.rb
@@ -1,6 +1,6 @@
 class Person
   def initialize(name="Unknown", age, parent_permission: true)
-    @id = Math.floor(Math.random * 10)
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
@@ -14,4 +14,17 @@ class Person
   # Setters
   attr_writer :name
   attr_writer :age
+
+  def can_use_services?
+    if @age >= 18 || @parent_permission
+      true
+    else
+      false
+    end
+  end
+
+  private
+  def is_of_age?
+    @age >= 18 ? true : false
+  end
 end

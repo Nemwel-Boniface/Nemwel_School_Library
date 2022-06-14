34f646bbb76d375c8b4a3689fd7d4179dc8741f1
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-06-14 12:48:45 +0300

    Update checked whether the decorators in my project work correctly. (They do)

--- person.rb
+++ person.rb
@@ -29,3 +29,10 @@ class Person < Nameable
     @age >= 18
   end
 end
+
+person = Person.new(22, 'maximilianus')
+puts person.correct_name
+capitalizedPerson = CapitalizeDecorator.new(person)
+puts capitalizedPerson.correct_name
+capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+puts capitalizedTrimmedPerson.correct_name

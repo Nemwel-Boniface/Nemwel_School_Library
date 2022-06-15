5edfc3e69ae2f701f24ced2f294bc85a2b8dcee8
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-06-15 12:50:03 +0300

    Update included;   - Create the has-many side of Book and Rental   - Modify the constructor of Rental so Book and Person are set in it.

--- rental.rb
+++ rental.rb
@@ -1,7 +1,20 @@
 class Rental
   attr_accessor :date
+  attr_reader :book, :person
 
-  def initialize(date)
+  def initialize(person, date, book)
     @date = date
+    @person = person
+    @book = book
+  end
+
+  def book=(book)
+    @book = book
+    book.rental.push(self) unless book.rental.includes?(self)
+  end
+
+  def person=(person)
+    @person = person
+    person.rental.push(self) unless person.rental.includes?(self)
   end
 end

acbd822f5dd936d96b153bf679bb3357747786cc
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-06-15 08:32:54 +0300

    Update Created a has-many relationship (a classroom has many students

--- classroom.rb
+++ classroom.rb
@@ -1,7 +1,13 @@
 class Classroom
   attr_accessor :label
+  attr_reader :students
 
   def initialize(label)
     @label = label
+    @students = []
+  end
+
+  def add_students(student)
+    @students.push(student)
   end
 end

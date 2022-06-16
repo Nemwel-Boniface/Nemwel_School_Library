5880bea39e97224287d5786f8253a4a2566621b0
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-06-16 05:08:37 +0300

    Update created the basic UI for functionality of the project

---
+++ app.rb
@@ -0,0 +1,8 @@
+puts "Welcome to my Library! You may perform the following tasks:
+1. List all books.
+2. List all people
+3. Create a person
+4. Create a book.
+5. Create a rental
+6. List all rentals for a given person id.
+7. Exit"
